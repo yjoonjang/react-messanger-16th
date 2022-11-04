b252546a16e13a4d0dbd886022fb33d394d2015b
yjoonjang <yjoonjang34@gmail.com>
2022-11-04 23:07:53 +0900

    feat: 이름 검색 기능 추가

--- src/components/profilePage/ProfilePage.tsx
+++ src/components/profilePage/ProfilePage.tsx
@@ -2,23 +2,57 @@ import { Row, Column } from '../elements/Wrapper.style';
 import UserBar from '../elements/UserBar';
 import { useRecoilValue } from 'recoil';
 import { userState } from '../../state/userState';
-// import { userInfoState } from '../../state/userState';
+import SearchIcon from '../../image/emoji/SearchIcon';
+import { useState } from 'react';
+import { IUser } from '../../state/userState';
 
 const ProfilePage = () => {
   const userList = useRecoilValue(userState);
+  const [newUserList, setNewUserList] = useState<IUser[]>(userList);
+  const [inputValue, setInputValue] = useState<string>('');
+  const [isSearchIconClicked, setIsSearchIconClicked] = useState<boolean>(false);
+
+  const onHandleInputText = (e: React.FormEvent<HTMLInputElement>) => {
+    e.preventDefault();
+    const text = e.currentTarget.value;
+
+    setInputValue(text);
+    let tempList = newUserList.filter((userList) => {
+      userList.username.includes(text);
+      return;
+    });
+
+    setNewUserList(tempList);
+  };
+
+  const onClickSearchIcon = () => {
+    setIsSearchIconClicked(!isSearchIconClicked);
+  };
+
   return (
     <>
       <Row width="100%" justifyContent="space-between" alignItems="start">
         <span style={{ fontWeight: '600' }}>파트짱</span>
-        {/* <span>QuestionMarkIcon</span> */}
+        <SearchIcon onClick={onClickSearchIcon} />
       </Row>
-      <Column>
-        <>
-          {userList.map((user) => {
-            return <UserBar key={user.userId} username={user.username} statusMessage={user.introMessage} />;
-          })}
-        </>
-      </Column>
+      {isSearchIconClicked ? (
+        <Column>
+          <input onChange={onHandleInputText} style={{ width: '100%', marginBottom: '12px' }} value={inputValue} />
+          <>
+            {newUserList.map((user) => {
+              return <UserBar key={user.userId} username={user.username} statusMessage={user.introMessage} />;
+            })}
+          </>
+        </Column>
+      ) : (
+        <Column>
+          <>
+            {userList.map((user) => {
+              return <UserBar key={user.userId} username={user.username} statusMessage={user.introMessage} />;
+            })}
+          </>
+        </Column>
+      )}
     </>
   );
 };
